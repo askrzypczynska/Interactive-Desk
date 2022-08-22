e39f2bbd51278a276c7cb0ba11ce0aafc7e61346
askrzypczynska <aleksandraskrzypczynska@gmail.com>
2022-08-22 17:53:02 +0200

    fixing Fonts on main site

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let zoom = Number((window.innerWidth / window.screen.width).toFixed(3));
 
-const changeSize = ()=>{
+const changeSize = () => {
 	zoom = Number((window.innerWidth / window.screen.width).toFixed(3));
 	document.firstElementChild.style.zoom = zoom;
 }
+
+changeSize()
 
 window.addEventListener('resize', changeSize);
